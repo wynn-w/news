bfb03cf54d285fd1447c19bbac6f02909e431406
wynn-w <wwwynn22@gmail.com>
2021-04-02 18:53:14 +0800

    修复：小程序兼容实现

--- pages/tabBar/follow/follow.vue
+++ pages/tabBar/follow/follow.vue
@@ -11,9 +11,8 @@
 		</view>
 		<!-- 主体 -->
 		<view class="follow__content">
-			<!-- <uni-load-more v-if="acticleLiks.length === 0 || acticleLiks.length  > (pageSize -1)" iconType="snow" :status="load"></uni-load-more> -->
-			<view class="follow__content__article" v-if="choose === 0">
-
+			<uni-load-more v-if="acticleLiks.length === 0 " iconType="snow" status="loading"></uni-load-more>
+			<view class="follow__content__article" :style="{height:height}" v-if="choose === 0">
 				<list-scroll :list="acticleLiks" @loadMore="loadMore" :load="load" :pageSize="pageSize" :current="choose"></list-scroll>
 			</view>
 			<view class="follow__content__author" v-else>
@@ -30,9 +29,11 @@
 			return {
 				choose: 0,
 				acticleLiks: [],
-				pageSize: 5,
+				pageSize: 6,
 				load: {},
-				loadMoreTag: false
+				loadMoreTag: false,
+				contentInfo: null,
+				height: `100px`
 			}
 		},
 		methods: {
@@ -43,7 +44,6 @@
 				this.choose = 1
 			},
 			updateActicle(arg) {
-				// const { flag = '' } = arg
 				this.$api.getArticleLikes({
 					// userId:'',
 					page: this.load[this.choose].page,
@@ -54,25 +54,19 @@
 						this.loadInit()
 					}
 					if (data.length === 0) {
-						console.log(122);
 						let old = this.load[this.choose]
 						old.loading = "noMore"
-						// old.page ='12222'
-						this.load=Object.assign({},this.load,old) 
-						// this.$forceUpdate()
+						this.load = Object.assign({}, this.load, old)
 						return
 					}
 					// loadMore 触发的更新
 					if (arg && arg.flag === "loadMore") {
-
 						this.acticleLiks.push(...data)
 						this.loadMoreTag = false
 						return
 					}
 					this.loadMoreTag = false
 					this.acticleLiks = data
-
-
 				})
 			},
 			loadMore() {
@@ -81,6 +75,7 @@
 				if (this.load[this.choose].loading === "noMore") return
 				this.load[this.choose].page++
 				this.updateActicle({ flag: 'loadMore' })
+
 			},
 			loadInit() {
 				this.load[this.choose] = {
@@ -97,6 +92,12 @@
 				this.updateActicle()
 			})
 			this.updateActicle()
+			// #ifndef H5 ||APP-PLUS ||MP-ALIPAY
+			let view = uni.createSelectorQuery().select('.follow__content__article')
+			view.fields({ rect: true }, data => {
+				this.height = `${data.top}px`
+			}).exec()
+			// #endif
 		},
 	}
 </script>
@@ -104,13 +105,14 @@
 <style lang="scss">
 	page {
 		height: 100%;
+		display: flex;
 	}
 
 	.news-follow {
-		height: 100%;
-		width: 100%;
 		display: flex;
 		flex-direction: column;
+		flex: 1;
+		box-sizing: border-box;
 
 
 		.follow__header {
@@ -124,12 +126,14 @@
 			.follow__header__item {
 				padding: 20rpx 0;
 				font-size: 32rpx;
-				font-weight: bold;
+				font-weight: 500;
+				color: #838383;
 				border-bottom: 8rpx solid #FFFFFF;
 
 				&.active {
 					color: $base-color;
 					border-bottom: 6rpx solid $base-color;
+					font-weight: 700;
 				}
 			}
 		}
@@ -138,11 +142,15 @@
 			display: flex;
 			flex-direction: column;
 			flex: 1;
-			overflow: hidden;
 
 			>.follow__content__article {
+				// display: flex;
 				flex: 1;
-				overflow: hidden;
+
+				.news-list__scroll {
+					height: 100%;
+				}
+
 			}
 
 			>.follow__content__author {}
