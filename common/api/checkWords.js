f92bd62e9aec8f23a89d0beb4193b6061d2ae563
wynn-w <wwwynn22@gmail.com>
2021-04-08 23:52:56 +0800

    修改：使用 uni.request 重写了 checkWords 方法，修复小程序请求报错问题

--- common/api/checkWords.js
+++ common/api/checkWords.js
@@ -1,9 +1,22 @@
 // 敏感词检测
-export  const checkWords = async function(words){
-	let url = `https://api.menglong.me/sensitive-words?text=${words}`
-	return await fetch(url,{ method: 'GET',authority: 'api.menglong.me'}).then(res=> {
-		return new Promise((reslove, reject) => {
-			reslove(res.json())
-		})
-	})
+export const checkWords = async function(words) {
+	let url = `https://api.menglong.me/sensitive-words`
+	return await uni.request({
+		url:url,
+		method:"POST",
+		data:{
+			text: words
+		},
+		header:{
+			"Referrer-Policy": "no-referrer",
+			"Content-Type":"application/json",
+		}
+	}).then(res =>res[1].data
+	).catch(err=> err)
+
+	// fetch(url,{ method: 'GET',authority: 'api.menglong.me'}).then(res=> {
+	// 	return new Promise((reslove, reject) => {
+	// 		reslove(res.json())
+	// 	})
+	// })
 }
