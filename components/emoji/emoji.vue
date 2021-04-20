fa382c3741f2c91acf598483bed47bdcaa2a753c
wynn-w <wwwynn22@gmail.com>
2021-04-20 17:55:21 +0800

    新增：抽离 emoji 表情逻辑并封装成独立组件

---
+++ components/emoji/emoji.vue
@@ -0,0 +1,72 @@
+<template>
+	<swiper class="emoji" v-if="emojiDataInited" :indicator-dots="true" :current="0">
+		<swiper-item v-for="(item, key) in emojiData" :key="key" class="emoji-emoji" :class="[key==emojiData.length-1?'lastbox':'']">
+			<text v-for="(emoji, index) in item" :key="index" @click="selectEmoji(emoji)" class="emoji-emoji-icon ">{{ emoji }} </text>
+		</swiper-item>
+	</swiper>
+</template>
+
+<script>
+	import emoji from "./emoji.js"
+	export default {
+		data() {
+			return {
+				emojiData: [], //表情包数组
+				emojiDataInited: false,
+			};
+		},
+		methods: {
+			/**
+			 * 表情包相关
+			 * */
+			selectEmoji(m) {
+				this.$emit('selectEmoji', m)
+			},
+			openEmoji() {
+				this.emojiDataInited = true
+			},
+			closeEmoji() {
+				this.emojiDataInited = false
+			},
+			InitEmoji(emoji) {
+				let page = Math.ceil(emoji.length / 42);
+				for (let i = 0; i < page; i++) {
+					this.emojiData[i] = [];
+					for (let k = 0; k < 42; k++) {
+						emoji[i * 42 + k] ? this.emojiData[i].push(
+							emoji[i * 42 + k]
+						) : ''
+					}
+				}
+			},
+		},
+		mounted() {
+			this.InitEmoji(emoji)
+		}
+	}
+</script>
+
+<style lang="scss">
+	.emoji {
+		box-sizing: border-box;
+		width:100vw;
+		height: 288rpx;
+
+		&-emoji {
+			box-sizing: border-box;
+			width: 100%;
+			flex-direction: row;
+			flex-wrap: wrap;
+			justify-content: center;
+
+			&-icon {
+				box-sizing: border-box;
+				width: 53%;
+				text-align: center;
+				padding: 10.5rpx 4rpx;
+				font-size: 44rpx;
+			}
+		}
+
+	}
+</style>
